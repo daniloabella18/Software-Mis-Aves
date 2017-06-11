56dce5d361c3c7ac217c22c02a9d2ff9c5cb1170
Sergio Salinas <sergio.salfer@gmail.com>
2017-06-11 02:35:42 -0200

    agregado view

--- dashboard.php
+++ dashboard.php
@@ -8,65 +8,14 @@ if($_SESSION["loggedin"] != true) {
 
 <!doctype html>
 <html lang="es">
-<head>
-	<meta charset="utf-8" />
-	<link rel="icon" type="image/png" href="assets/img/favicon.ico">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-
-	<title>Mis aves</title>
-
-	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
-    <meta name="viewport" content="width=device-width" />
-
-
-    <!-- Bootstrap core CSS     -->
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
-
-    <!-- Animation library for notifications   -->
-    <link href="assets/css/animate.min.css" rel="stylesheet"/>
-
-    <!--  Light Bootstrap Table core CSS    -->
-    <link href="assets/css/light-bootstrap-dashboard.css" rel="stylesheet"/>
-
-
-    <!--  CSS for Demo Purpose, don't include it in your project     -->
-    <link href="assets/css/demo.css" rel="stylesheet" />
-
-
-    <!--     Fonts and icons     -->
-    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-    <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
-    <link href="assets/css/pe-icon-7-stroke.css" rel="stylesheet" />
-
-</head>
+<?php
+include('view/head.php');
+?>
 <body>
 <div class="wrapper">
-    <div class="sidebar" data-color="dark" data-image="assets/img/sidebar-6.jpg">
-    	<div class="sidebar-wrapper">
-            <div class="logo">
-                <a href="http://xvideos.com" class="simple-text">
-									<img class="data-image" src="assets/img/logo.png" alt="logo" width="50">
-                    Mis aves
-                </a>
-            </div>
-
-            <ul class="nav">
-                <li class="active">
-                    <a href="dashboard.php">
-                        <i class="pe-7s-graph"></i>
-                        <p>Dashboard</p>
-                    </a>
-                </li>
-								<li>
-										<a href="ave.php">
-												<i class="pe-7s-graph"></i>
-												<p>Ave</p>
-										</a>
-								</li>
-            </ul>
-    	</div>
-    </div>
-
+	<?php
+	include('view/sidebar.php');
+	?>
     <div class="main-panel">
         <nav class="navbar navbar-default navbar-fixed">
             <div class="container-fluid">

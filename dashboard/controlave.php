c91239b7f816380405d4edde31d368093adfb286
Sergio Salinas <sergio.salfer@gmail.com>
2017-06-26 11:49:23 -0400

    agregada consulta busqueda registro

--- dashboard/controlave.php
+++ dashboard/controlave.php
@@ -60,11 +60,89 @@ include '../layouts/head.php';
 
     <!--Main layout-->
     <main>
-    <div class="container-fluid text-center">
 
-      <div class="card card-block">
 
-    </div>
+      <div class="container-fluid text-center">
+
+        <div class="card card-block">
+          <h4 class="card-title">Buscar Control</h4>
+          <p class="card-text">Busqueda de los últimos 6 controles de un ave</p>
+          <form action="" method="GET">
+              <div class="row">
+                <div class="col-md-4 offset-md-4" >
+                  <div class="md-form input-group">
+                    <input type="search" class="form-control" placeholder="Anillo o Nombre del ave" name="search">
+                    <span class="input-group-btn">
+                        <button   type="submit" class="btn btn-primary btn-lg" type="button"><i class="fa fa-search" aria-hidden="true"></i></button>
+                    </span>
+                </div>
+                </div>
+              </div>
+              </form>
+          <br/>
+          <form action="controlave.php" method="post">
+
+          <?php
+
+          //Se recibe la información de buscar un ave
+          if (isset($_GET['search'])) {
+            //SELECT C.Con_id, A.Ave_anillo, A.Ave_nombre, C.Con_peso,C.Con_cape, U.usu_nombre, C.Con_fecha, T.Tur_descp, C.Con_obs  FROM control C, ave A, usuario U, turno T WHERE C.Con_Ave = A.Ave_anillo and C.Con_usu = U.usu_rut and C.Con_turno = T.Tur_cod
+               $sql = "SELECT C.Con_id, A.Ave_anillo, A.Ave_nombre, C.Con_peso,C.Con_cape, U.usu_nombre, C.Con_fecha, T.Tur_descp, C.Con_obs  FROM control C, ave A, usuario U, turno T WHERE C.Con_Ave = A.Ave_anillo and C.Con_usu = U.usu_rut and C.Con_turno = T.Tur_cod
+                        and (C.Con_Ave = '".$_GET['search']."' or A.Ave_nombre = '".$_GET['search']."')";
+               $result = $conexion->query($sql);
+               $count = 1;
+              if(mysqli_num_rows($result) ==0){
+                echo 'No hay controles registrados para el ave '.$_GET['search'];
+              }else{
+                   echo '<div class="table-responsive">
+                        <table class="table ">
+                   <thead>
+                   <th class="addr" id="table_id" ></th>
+                   <th>ID</th>
+                   <th>Anillo</th>
+                   <th>Peso</th>
+                   <th>Caperuza</th>
+                   <th>Cetrero</th>
+                   <th>Fecha</th>
+                   <th>Turno</th>
+                   <th>Observación</th>
+                   </thead>
+                   <tbody>';
+
+               while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
+                 //if($_GET['search'] == $row['ave_anillo'] or $_GET['search'] == $row['ave_nombre']){
+
+                   echo '<tr>';
+                   echo '<td><fieldset class="form-group"><input type="checkbox" id="checkbox'.$count.'" name="data['.$count.'][checkbox]" value="on "><label for="checkbox'.$count.'"></label></fieldset></td>';
+                   echo '<td ><input type="hidden" name="data['.$count.'][ave_anillo]" value="'.$row['ave_anillo'].'">'.$row['ave_anillo'].'</td>';
+                   echo '<td><input type="hidden" name="data['.$count.'][ave_nombre]" value="'.$row['ave_nombre'].'">'.$row['ave_nombre'].'</td>';
+                   echo '<td><input type="hidden" name="data['.$count.'][est_descrip]" value="'.$row['est_descrip'].'">'.$row['est_descrip'].'</td>';
+                   echo '<td><input type="hidden" name="data['.$count.'][Ave_fecha_nac]" value="'.$row['Ave_fecha_nac'].'">'.date("d-m-Y", strtotime($row['Ave_fecha_nac'])).'</td>';
+                   echo '<td><input type="hidden" name="data['.$count.'][esp_nombre]" value="'.$row['esp_nombre'].'">'.$row['esp_nombre'].'</td>';
+                   echo '<td><input type="hidden" name="data['.$count.'][ave_genero]" value="'.$row['ave_genero'].'">'.$row['ave_genero'].'</td>';
+                   echo "</tr>";
+
+                $count= $count + 1;
+                //}
+               }
+               echo '
+                      </tbody>
+                  </table>
+                  </div>';
+              //Botton de modificar ave
+               echo '<div class="md-form form-group">';
+               echo '<input type="hidden" name="count" value="data">';
+               echo '<button type="submit"  name="modificar" class="btn btn-primary btn-lg">Modificar</button>';
+               //Botton de quitar ave
+               echo '<input type="hidden" name="count" value="data">';
+               echo '<button type="submit"  name="quitar" class="btn btn-primary btn-lg">Quitar</button>';
+               echo "</div>";
+             }
+           }
+              ?>
+            </form>
+        </div>
+
 
     <br>
 
